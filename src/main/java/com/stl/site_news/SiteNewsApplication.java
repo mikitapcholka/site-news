c2cdbec0c519e7aa2346f2a5a062c90239e743cb
MikitaPcholka <mikita.pcholka@gmail.com>
2025-02-18 16:53:59 +0300

    first outline

---
+++ src/main/java/com/stl/site_news/SiteNewsApplication.java
@@ -0,0 +1,13 @@
+package com.stl.site_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stl/site_news/SiteNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stl.site_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
