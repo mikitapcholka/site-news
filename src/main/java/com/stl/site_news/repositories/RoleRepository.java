8f95842d4d47046a910f8af9bf0b4d0231c806e7
MikitaPcholka <mikita.pcholka@gmail.com>
2025-04-10 19:19:46 +0300

    clean up

--- src/main/java/com/stl/site_news/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stl.site_news.repositories;
-
-import com.stl.site_news.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
